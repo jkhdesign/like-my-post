2c6b889513e13736016e4d4d56fcc5cf9186aadf
Josh Krauth-Harding <im.joshkh@gmail.com>
2019-04-29 16:18:19 -0400

    Mobile enhancement

--- js/drawEmojis.js
+++ js/drawEmojis.js
@@ -1,7 +1,14 @@
+let emojisLeft = 195;
+let maxEmojis = 195;
+
 let emojis = [];
 let shakeCount = 0;
 
 function initShakeListener() {
+
+  $("#total-used").html(emojisLeft);
+  $("#total-available").html(maxEmojis);
+
   let shakeEvent = new Shake({
     threshold: 15
   });
@@ -13,17 +20,28 @@ function initShakeListener() {
 }
 
 function shake() {
-  emojis = [];
-  for (let i = 0; i < Math.max((50 / (1 + Math.pow((shakeCount + 2) / 5, 5))), 1); i++) {
-    emojis.push(new Emoji(Math.random(), Math.random()));
+  if (emojisLeft <= 0) {
+    if (!($("#reset").length)) {
+      console.log("No more emoji! Creating reset button");
+      $("body").append("<h2 id=\"reset\" onclick=\"reset()\">Post again?</h2>");
+    } else {
+      console.log("No more emoji, + button already exists; doing nothing");
+    }
+  } else {
+    emojis = [];
+    for (let i = 0; i < Math.max((50 / (1 + Math.pow((shakeCount + 2) / 5, 5))), 1); i++) {
+      emojis.push(new Emoji(Math.random(), Math.random()));
+    }
+    for (let emoji of emojis) {
+      setTimeout(() => {
+        $("#emoji-container").append(`<div class="emoji ${emoji.sprite}" data-x="${emoji.x}" data-y="${emoji.y}"></div>`);
+        emojisLeft--;
+        $("#total-used").html(emojisLeft);
+        redrawEmojis();
+      }, Math.random() * 1000);
+    }
+    shakeCount++;
   }
-  for (let emoji of emojis) {
-    setTimeout(() => {
-      $("#emoji-container").append(`<div class="emoji ${emoji.sprite}" data-x="${emoji.x}" data-y="${emoji.y}"></div>`);
-      redrawEmojis();
-    }, Math.random() * 1000);
-  }
-  shakeCount++;
 }
 
 function redrawEmojis() {
@@ -31,6 +49,14 @@ function redrawEmojis() {
     $(emoji).css("left", ($(emoji).attr("data-x") * window.innerWidth - 48));
     $(emoji).css("top", $(emoji).attr("data-y") * window.innerHeight - 48);
   });
+}
+
+function reset() {
+  emojisLeft = 195;
+  $("#total-used").html(emojisLeft);
+  $("#total-available").html(maxEmojis);
+  $('#reset').remove();
+  shakeCount = 0;
 }
 
 class Emoji {
